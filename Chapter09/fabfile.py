8eb26a1e4390a9e41c24c8f3872773b8334965d2
showa_yojyo <yojyo@hotmail.com>
2020-05-08 11:45:03 +0900

    Read Chapter09

--- Chapter09/fabfile.py
+++ Chapter09/fabfile.py
@@ -1,8 +1,11 @@
 # Code Listing #2
 
+# Fablic は SSH によるシェルコマンドをリモート実行するための
+# サードパーティー製パッケージ。
 from fabric.api import run
 
-
+# したがって次のコードはパッケージ application を
+# pip でインストールするという処理を意味する。
 def remote_install(application):
 
     print('Installing', application)
